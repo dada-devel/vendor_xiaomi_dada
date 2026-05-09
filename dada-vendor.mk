4fd211bc1537fca9b55b020c189c9df4e9609f8b
karthick-kk <kkzone@gmail.com>
2026-05-09 12:12:22 +0530

    dada: initial vendor tree

---
+++ dada-vendor.mk
@@ -0,0 +1,49 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/xiaomi/dada
+
+PRODUCT_COPY_FILES += \
+    vendor/xiaomi/dada/proprietary/odm/etc/audio-test-config:$(TARGET_COPY_OUT_ODM)/etc/audio-test-config \
+    vendor/xiaomi/dada/proprietary/odm/etc/audio/RES1K.wav:$(TARGET_COPY_OUT_ODM)/etc/audio/RES1K.wav \
+    vendor/xiaomi/dada/proprietary/odm/etc/audio/audio_module_config_primary.xml:$(TARGET_COPY_OUT_ODM)/etc/audio/audio_module_config_primary.xml \
+    vendor/xiaomi/dada/proprietary/odm/etc/audio/bottom_spk.wav:$(TARGET_COPY_OUT_ODM)/etc/audio/bottom_spk.wav \
+    vendor/xiaomi/dada/proprietary/odm/etc/audio/bottom_spk_hp.wav:$(TARGET_COPY_OUT_ODM)/etc/audio/bottom_spk_hp.wav \
+    vendor/xiaomi/dada/proprietary/odm/etc/audio/hrtf5c.bin:$(TARGET_COPY_OUT_ODM)/etc/audio/hrtf5c.bin \
+    vendor/xiaomi/dada/proprietary/odm/etc/audio/mis_head.bin:$(TARGET_COPY_OUT_ODM)/etc/audio/mis_head.bin \
+    vendor/xiaomi/dada/proprietary/odm/etc/audio/mis_head_spatial.bin:$(TARGET_COPY_OUT_ODM)/etc/audio/mis_head_spatial.bin \
+    vendor/xiaomi/dada/proprietary/odm/etc/audio/mis_powerop_configs.bin:$(TARGET_COPY_OUT_ODM)/etc/audio/mis_powerop_configs.bin \
+    vendor/xiaomi/dada/proprietary/odm/etc/audio/mis_spk.bin:$(TARGET_COPY_OUT_ODM)/etc/audio/mis_spk.bin \
+    vendor/xiaomi/dada/proprietary/odm/etc/audio/mis_spk_spatial.bin:$(TARGET_COPY_OUT_ODM)/etc/audio/mis_spk_spatial.bin \
+    vendor/xiaomi/dada/proprietary/odm/etc/audio/misound_karaoke_res.bin:$(TARGET_COPY_OUT_ODM)/etc/audio/misound_karaoke_res.bin \
+    vendor/xiaomi/dada/proprietary/odm/etc/audio/misound_karaokemix_res.bin:$(TARGET_COPY_OUT_ODM)/etc/audio/misound_karaokemix_res.bin \
+    vendor/xiaomi/dada/proprietary/odm/etc/audio/misoundsa_spk_config.bin:$(TARGET_COPY_OUT_ODM)/etc/audio/misoundsa_spk_config.bin \
+    vendor/xiaomi/dada/proprietary/odm/etc/audio/rcv.wav:$(TARGET_COPY_OUT_ODM)/etc/audio/rcv.wav \
+    vendor/xiaomi/dada/proprietary/odm/etc/audio/rcv_hp.wav:$(TARGET_COPY_OUT_ODM)/etc/audio/rcv_hp.wav \
+    vendor/xiaomi/dada/proprietary/odm/etc/audio/rcv_seal.wav:$(TARGET_COPY_OUT_ODM)/etc/audio/rcv_seal.wav \
+    vendor/xiaomi/dada/proprietary/odm/etc/audio/rcv_seal_arithmetic.wav:$(TARGET_COPY_OUT_ODM)/etc/audio/rcv_seal_arithmetic.wav \
+    vendor/xiaomi/dada/proprietary/odm/etc/audio/silent-3sec.wav:$(TARGET_COPY_OUT_ODM)/etc/audio/silent-3sec.wav \
+    vendor/xiaomi/dada/proprietary/odm/etc/audio/sku_sun/adsp_sleep_monitor.conf:$(TARGET_COPY_OUT_ODM)/etc/audio/sku_sun/adsp_sleep_monitor.conf \
+    vendor/xiaomi/dada/proprietary/odm/etc/audio/sku_sun/mixer_paths_overlay_dynamic.xml:$(TARGET_COPY_OUT_ODM)/etc/audio/sku_sun/mixer_paths_overlay_dynamic.xml \
+    vendor/xiaomi/dada/proprietary/odm/etc/audio/sku_sun/mixer_paths_overlay_static.xml:$(TARGET_COPY_OUT_ODM)/etc/audio/sku_sun/mixer_paths_overlay_static.xml \
+    vendor/xiaomi/dada/proprietary/odm/etc/audio/sku_sun/mixer_paths_sun_mtp.xml:$(TARGET_COPY_OUT_ODM)/etc/audio/sku_sun/mixer_paths_sun_mtp.xml \
+    vendor/xiaomi/dada/proprietary/odm/etc/audio/sku_sun/resourcemanager_sun_mtp.xml:$(TARGET_COPY_OUT_ODM)/etc/audio/sku_sun/resourcemanager_sun_mtp.xml \
+    vendor/xiaomi/dada/proprietary/odm/etc/audio/sku_sun_qssi/audio_policy_configuration.xml:$(TARGET_COPY_OUT_ODM)/etc/audio/sku_sun_qssi/audio_policy_configuration.xml \
+    vendor/xiaomi/dada/proprietary/odm/etc/audio/spk.wav:$(TARGET_COPY_OUT_ODM)/etc/audio/spk.wav \
+    vendor/xiaomi/dada/proprietary/odm/etc/audio/test.wav:$(TARGET_COPY_OUT_ODM)/etc/audio/test.wav \
+    vendor/xiaomi/dada/proprietary/odm/etc/audio/top_spk.wav:$(TARGET_COPY_OUT_ODM)/etc/audio/top_spk.wav \
+    vendor/xiaomi/dada/proprietary/odm/etc/audio/top_spk_hp.wav:$(TARGET_COPY_OUT_ODM)/etc/audio/top_spk_hp.wav \
+    vendor/xiaomi/dada/proprietary/odm/etc/audio_base.db:$(TARGET_COPY_OUT_ODM)/etc/audio_base.db \
+    vendor/xiaomi/dada/proprietary/odm/etc/audio_policy_engine_configuration_mi.xml:$(TARGET_COPY_OUT_ODM)/etc/audio_policy_engine_configuration_mi.xml \
+    vendor/xiaomi/dada/proprietary/odm/etc/audio_policy_engine_default_stream_volumes_mi.xml:$(TARGET_COPY_OUT_ODM)/etc/audio_policy_engine_default_stream_volumes_mi.xml \
+    vendor/xiaomi/dada/proprietary/odm/etc/audio_policy_engine_product_strategies_mi.xml:$(TARGET_COPY_OUT_ODM)/etc/audio_policy_engine_product_strategies_mi.xml \
+    vendor/xiaomi/dada/proprietary/odm/etc/audio_policy_engine_stream_volumes_mi.xml:$(TARGET_COPY_OUT_ODM)/etc/audio_policy_engine_stream_volumes_mi.xml \
+    vendor/xiaomi/dada/proprietary/odm/etc/init/vendor.xiaomi.hardware.vibratorfeature.service.rc:$(TARGET_COPY_OUT_ODM)/etc/init/vendor.xiaomi.hardware.vibratorfeature.service.rc \
+    vendor/xiaomi/dada/proprietary/odm/etc/permissions/vendor.hardware.vibratorfeature-permission.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/vendor.hardware.vibratorfeature-permission.xml
+
+PRODUCT_PACKAGES += \
+    libaachaptics \
+    vendor.xiaomi.hardware.vibratorfeature.service.xml \
+    vendor.xiaomi.hardware.vibratorfeature.service
